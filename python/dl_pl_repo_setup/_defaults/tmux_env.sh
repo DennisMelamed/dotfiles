bc44c52dfd11aa0db1751ea87a0356160af5210d
Dennis Melamed <dennis.melamed@kitware.com>
2022-11-22 13:40:05 -0600

    improved tmux env

--- python/dl_pl_repo_setup/_defaults/tmux_env.sh
+++ python/dl_pl_repo_setup/_defaults/tmux_env.sh
@@ -13,12 +13,22 @@ then
 
     # Name first Pane and start zsh
     tmux rename-window -t 0 'Main'
-    tmux split-window -vf -t 'Main'
 #    tmux send-keys -t 'Main' 'zsh' C-m 'clear' C-m # Switch to bind script?
+    tmux new-window -t $SESSION:1 -n 'Data'
+    tmux send-keys -t 'Data' 'cd ./data' C-m 
+    tmux new-window -t $SESSION:2 -n 'Models'
+    tmux send-keys -t 'Models' 'cd ./models' C-m 
+    tmux new-window -t $SESSION:3 -n 'Configs'
+    tmux send-keys -t 'Configs' 'cd ./configs' C-m 
 
     # Create and setup pane for tensorboard
-    tmux new-window -t $SESSION:1 -n 'Tensorboard'
-    tmux send-keys -t 'Tensorboard' 'source *_venv/bin/activate && tensorboard --logdir=./experiments' C-m # Switch to bind script?
+    tmux new-window -t $SESSION:4 -n 'Monitors'
+    tmux send-keys -t 'Monitors' 'source *_venv/bin/activate && tensorboard --logdir=./experiments' C-m 
+    tmux split-window -vf -t "Monitors"
+    tmux send-keys -t 'Monitors.1' "watch -n 10 'df -h'" C-m 
+    tmux resize-pane -t 'Monitors.1' -U 10
+    tmux split-window -hf -t "Monitors"
+    tmux send-keys -t 'Monitors.2' 'watch -n 3 nvidia-smi' C-m 
 
 fi
 
