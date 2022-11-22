4d9daec51e9532228c91e896847cc92b10efb8a2
Dennis Melamed <dennis.melamed@kitware.com>
2022-11-22 13:56:15 -0600

    improved tmux env

--- python/dl_pl_repo_setup/_defaults/tmux_env.sh
+++ python/dl_pl_repo_setup/_defaults/tmux_env.sh
@@ -14,11 +14,15 @@ then
     # Name first Pane and start zsh
     tmux rename-window -t 0 'Main'
 #    tmux send-keys -t 'Main' 'zsh' C-m 'clear' C-m # Switch to bind script?
+    tmux send-keys -t 'Main' 'source *_venv/bin/activate' C-m
     tmux new-window -t $SESSION:1 -n 'Data'
+    tmux send-keys -t 'Data' 'source *_venv/bin/activate' C-m
     tmux send-keys -t 'Data' 'cd ./data' C-m 
     tmux new-window -t $SESSION:2 -n 'Models'
+    tmux send-keys -t 'Models' 'source *_venv/bin/activate' C-m
     tmux send-keys -t 'Models' 'cd ./models' C-m 
     tmux new-window -t $SESSION:3 -n 'Configs'
+    tmux send-keys -t 'Configs' 'source *_venv/bin/activate' C-m
     tmux send-keys -t 'Configs' 'cd ./configs' C-m 
 
     # Create and setup pane for tensorboard
